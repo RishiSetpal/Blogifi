1dbcb708f0fab7dd92d8070b2929fc1e9d646428
RishiSetpalAcerNitro <rishisetpal123@gmail.com>
2025-01-14 18:58:31 +0530

    Scope 1 Part2: Storing it in Local Memory Adding All Other https methods get, put, post, get(with id), patch, delete

--- src/main/java/com/example/Blogifi/controller/PostController.java
+++ src/main/java/com/example/Blogifi/controller/PostController.java
@@ -12,11 +12,16 @@ import java.util.Collection;
 public class PostController {
 
     // AutoWired will Help to use the IOC (inversion of Control) i.e. Spring container objects direct binding to this object
-    // instead of calling Post Constructor Post(its Parameters)
+    // instead of calling Post Constructor Post(its Parameters) it auto Construction Injection
     // ex. new Post(int id, String title, String desc, LinkedHashSet<String> tags)
 
-   @Autowired
-   PostService postService;
+    // making it final and private so that it can not be accessed by others directly.
+    @Autowired
+    final private PostService postService;
+
+    PostController(PostService postService){
+        this.postService=postService;
+    }
 
     @GetMapping("")
     public Collection<Post> getAllPosts(){
@@ -27,6 +32,35 @@ public class PostController {
     @PostMapping("")
     public String createPosts(@RequestBody Post post){
 //        return "Posts Created";
-        return postService.create(post);
+        postService.create(post);
+        return "Post Created Successfully";
     }
+
+    @GetMapping("/{id}")
+    public Post GetPostById(@PathVariable int id){
+        return postService.getpost(id);
+    }
+
+    @PutMapping("/{id}")
+    public String UpdatePostById(@PathVariable int id, @RequestBody Post post){
+        postService.Update(id,post);
+        return "Post Updated Successfully";
+    }
+
+    @PatchMapping("/{id}")
+    public String UpdatePartialPostById(@PathVariable int id,@RequestBody Post post ){
+        Post oldpost=postService.getpost(id);
+        oldpost.setTitle(post.getTitle() !=null ? post.getTitle():oldpost.getTitle());
+        oldpost.setDesc(post.getDesc() !=null ? post.getTitle() :oldpost.getDesc());
+        oldpost.setTags(post.getTags() !=null ? post.getTags() : oldpost.getTags());
+        postService.Update(id,oldpost);
+        return "Post Updated Successfully";
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteById(@PathVariable int id){
+        postService.delete(id);
+        return "Post Deleted Successfully";
+    }
+
 }
--- src/main/java/com/example/Blogifi/services/PostService.java
+++ src/main/java/com/example/Blogifi/services/PostService.java
@@ -1,7 +1,9 @@
 package com.example.Blogifi.services;
 
 import com.example.Blogifi.enteties.Post;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.*;
 
@@ -15,12 +17,32 @@ public class PostService {
             )
     );
 
-    public String create(Post post) {
+    public void create(Post post) {
         posts.put(post.getId(), post);
-        return null;
     }
 
     public Collection<Post> getAll() {
         return posts.values();
     }
+
+
+    public Post getpost(int id){
+        Post post= posts.get(id);
+        if (post==null){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"User with Id " + id+" Not Found");
+        }
+        return post;
+    }
+
+    public void Update(int id,Post post){
+        getpost(id);
+        post.setId(id);
+        posts.put(post.getId(),post);
+    }
+
+    public void delete(int id){
+        getpost(id);
+        posts.remove(id);
+    }
+
 }
