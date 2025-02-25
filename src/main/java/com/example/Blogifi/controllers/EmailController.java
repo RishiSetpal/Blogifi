31dead9d900c8e81befc19cd239ca84e055703d7
RishiSetpal <rishisetpal123@gmail.com>
2025-02-25 08:19:06 +0530

    Part20 - Email Sending - Enabling 2Factor Authentication - Spring-boot-starter-mail - create a app password - use the app password in yml or in Environment Variables

---
+++ src/main/java/com/example/Blogifi/controllers/EmailController.java
@@ -0,0 +1,24 @@
+package com.example.Blogifi.controllers;
+
+import com.example.Blogifi.services.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class EmailController {
+    @Autowired
+    EmailService emailService;
+
+    @GetMapping("/email/sent")
+    public ResponseEntity<String> sentEmail(
+            @RequestParam String to,
+            @RequestParam String subject,
+            @RequestParam String body
+    ){
+        emailService.sendEmail(to, subject, body);
+        return ResponseEntity.ok("Email sent Successfully to"+to);
+    }
+}
---
+++ src/main/java/com/example/Blogifi/services/EmailService.java
@@ -0,0 +1,26 @@
+package com.example.Blogifi.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @Value("spring.mail.username")
+    private String email;
+
+    public void sendEmail(String to, String subject, String body){
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+        message.setFrom(email);
+        javaMailSender.send(message);
+    }
+
+}
