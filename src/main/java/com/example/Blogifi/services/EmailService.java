c4c29c4320f8fa52f022ff303b0e0416b94f8d4b
RishiSetpal <rishisetpal123@gmail.com>
2025-02-26 15:59:01 +0530

    Part21 - Updating Mail

--- src/main/java/com/example/Blogifi/services/EmailService.java
+++ src/main/java/com/example/Blogifi/services/EmailService.java
@@ -1,9 +1,12 @@
 package com.example.Blogifi.services;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,12 +18,25 @@ public class EmailService {
     private String email;
 
     public void sendEmail(String to, String subject, String body){
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(body);
-        message.setFrom(email);
-        javaMailSender.send(message);
+//        SimpleMailMessage message = new SimpleMailMessage();
+//        message.setTo(to);
+//        message.setSubject(subject);
+//        message.setText(body);
+//        message.setFrom(email);
+//        javaMailSender.send(message);
+
+        try {
+            MimeMessage message = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8") ;
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(body, true); // 'true' enables HTML content
+            helper.setFrom(email);
+
+            javaMailSender.send(message);
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
